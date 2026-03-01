daded24f9426bb1ac9d2ad6f02905e9702e071a6
isabelle <isabelle901@proton.me>
2026-03-01 13:20:42 -0800

    working physical memory allocator

--- kernel/src/memory/physical/bitmap_alloc.c
+++ kernel/src/memory/physical/bitmap_alloc.c
@@ -42,12 +42,14 @@ bool page_free(uint64_t page_index) {
 }
 
 void set_page(uint64_t page_index, bool free) {
+
   uint64_t byte = page_index / 8;
   uint8_t bit = page_index % 8;
 
   if (free) {
     page_bitmap[byte] &= ~(1 << bit);
   } else {
+    k_debug("physical page %d allocated", page_index);
     page_bitmap[byte] |= (1 << bit);
     last_page_used = page_index;
   }
@@ -79,7 +81,7 @@ uint64_t get_free_page() {
 
 void generate_page_mapping() {
   uint64_t pages_used = 0;
-  for (uint32_t i = 0; i < desc.chunk_count; i++) {
+  for (uint32_t i = 0; i <= desc.chunk_count; i++) {
     struct contiguous_memory_chunk chunk = desc.chunk_ptr[i];
 
     page_mappings[i].offset = chunk.base;
@@ -91,11 +93,40 @@ void generate_page_mapping() {
   }
 }
 
-void *pp_alloc() {
-  uint64_t index = get_free_page();
+uint64_t ptr_to_page(void *ptr) {
+  struct page_mapping mapping;
+  bool wrapped = false;
 
+retry:
+  mapping = page_mappings[last_used_mapping];
+
+  uint64_t segment_size =
+      (mapping.page_end_index - mapping.page_start_index) * PAGE_SIZE;
+
+  if (ptr >= (void *)mapping.offset &&
+      ptr < (void *)(mapping.offset + segment_size)) {
+
+    return mapping.page_start_index +
+           (uint64_t)(ptr - mapping.offset) / PAGE_SIZE;
+
+  } else if (last_used_mapping <= desc.chunk_count) {
+    last_used_mapping++;
+    goto retry;
+  }
+
+  if (!wrapped) {
+    last_used_mapping = 0;
+    wrapped = true;
+    goto retry;
+  }
+
+  return -1;
+}
+
+void *page_to_ptr(uint64_t page_index) {
   struct page_mapping mapping;
   uint64_t prev_end;
+  bool wrapped = false;
 
 retry:
   mapping = page_mappings[last_used_mapping];
@@ -106,18 +137,42 @@ retry:
     prev_end = 0;
   }
 
-  if (index >= mapping.page_start_index && index <= mapping.page_end_index) {
-    k_debug("page %d allocated to %p.", index,
-            ((index - prev_end) * PAGE_SIZE) + mapping.offset);
+  if (page_index >= mapping.page_start_index &&
+      page_index <= mapping.page_end_index) {
+    k_debug("page %d is at %p.", page_index,
+            ((page_index - prev_end) * PAGE_SIZE) + mapping.offset);
 
-    return (void *)((index - prev_end) * PAGE_SIZE) + mapping.offset;
+    return (void *)(mapping.offset +
+                    (page_index - mapping.page_start_index) * PAGE_SIZE);
 
-  } else if (last_used_mapping < desc.chunk_count) {
+  } else if (last_used_mapping <= desc.chunk_count) {
     last_used_mapping++;
     goto retry;
   }
 
+  if (!wrapped) {
+    last_used_mapping = 0;
+    wrapped = true;
+    goto retry;
+  }
+
+  k_err("Failed to allocate physical page %d!", page_index);
+  k_debug("last used mapping: %d", last_used_mapping);
+
   return NULL;
+}
+
+void *_pp_alloc(uint64_t page_index) {
+  if (page_free(page_index)) {
+    set_page(page_index, USED);
+    return page_to_ptr(page_index);
+  }
+  return NULL;
+}
+
+void *pp_alloc() {
+  uint64_t index = get_free_page();
+  return _pp_alloc(index);
 }
 
 void setup_physical_paging() {
@@ -131,15 +186,8 @@ void setup_physical_paging() {
   }
 
   pp_alloc();
-  last_page_used = 608;
   pp_alloc();
-  last_page_used = 1048;
   pp_alloc();
-  last_page_used = 77;
   pp_alloc();
-  last_page_used = 56000;
-  char *p = pp_alloc();
-  p = "hello memory!";
-
-  printf_("%s", p);
+  pp_alloc();
 }
--- kernel/src/memory/physical/header/bitmap_alloc.h
+++ kernel/src/memory/physical/header/bitmap_alloc.h
@@ -2,11 +2,19 @@
 #define BITMAP_ALLOC_H_
 
 #include <stdint.h>
+#include <stdbool.h>
 
 void setup_physical_paging();
+void *pp_alloc();
+void *_pp_alloc(uint64_t page_index);
+void* page_to_ptr(uint64_t page_index);
+uint64_t ptr_to_page(void *ptr);
+uint64_t get_free_page();
+void set_page(uint64_t page_index, bool free);
+bool page_free(uint64_t page_index);
 
 struct page_mapping {
-    uint32_t offset;
+    uint64_t offset;
     uint64_t page_start_index;
     uint64_t page_end_index;
 };
